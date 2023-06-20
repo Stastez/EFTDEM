9fdc52a658fa474d074b20a28dc2d4479a6d93bb
Sandro Steeger <sandro.steeger.stuff@gmail.com>
2023-06-21 00:36:09 +0200

    Adds linux installers for X11

---
+++ build_config.sh
@@ -0,0 +1,42 @@
+depFolder="deps"
+debugFolder="build-debug"
+releaseFolder="build-release"
+
+if [ -d "$depFolder" ]; then
+    rm -rf "build"
+fi
+
+cmake   -B build-debug \
+        -S . \
+        -D CMAKE_TOOLCHAIN_FILE="$depFolder/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+        -D glfw3_DIR="$depFolder/glfw/lib/cmake/glfw3" \
+        -D glbinding_DIR="$depFolder/glbinding" \
+        -D glm_DIR="$depFolder/glm/lib/cmake/glm" \
+        -D magic_enum_DIR="$depFolder/magic_enum/lib/cmake/magic_enum" \
+        -D yaml-cpp_DIR="$depFolder/yaml-cpp/lib/cmake/yaml-cpp" \
+        -G "Unix Makefiles"
+
+cmake   -B build-release \
+        -S . \
+        -D CMAKE_TOOLCHAIN_FILE="$depFolder/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+        -D glfw3_DIR="$depFolder/glfw/lib/cmake/glfw3" \
+        -D glbinding_DIR="$depFolder/glbinding" \
+        -D glm_DIR="$depFolder/glm/lib/cmake/glm" \
+        -D magic_enum_DIR="$depFolder/magic_enum/lib/cmake/magic_enum" \
+        -D yaml-cpp_DIR="$depFolder/yaml-cpp/lib/cmake/yaml-cpp" \
+        -G "Unix Makefiles"
+
+mkdir "$debugFolder"
+mkdir "$releaseFolder"
+
+cp "$depFolder/glfw/lib/libglfw.so" "$debugFolder"
+cp "$depFolder/glfw/lib/libglfw.so" "$releaseFolder"
+
+cp "$depFolder/glm/build-debug/glm/libglm_shared.so" "$debugFolder"
+cp "$depFolder/glm/build-release/glm/libglm_shared.so" "$releaseFolder"
+
+cp "$depFolder/glbinding/lib/libglbindingd.so" "$depFolder/glbinding/lib/libglbinding-auxd.so" -t "$debugFolder"
+cp "$depFolder/glbinding/lib/libglbinding.so" "$depFolder/glbinding/lib/libglbinding-aux.so" -t "$releaseFolder"
+
+cp "$depFolder/yaml-cpp/lib/libyaml-cppd.so" "$debugFolder"
+cp "$depFolder/yaml-cpp/lib/libyaml-cpp.so" "$releaseFolder"
