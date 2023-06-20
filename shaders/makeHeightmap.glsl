a6bf77590135d343551f879a671b7623a74d69b6
Sandro Steeger <sandro.steeger.stuff@gmail.com>
2023-06-20 16:58:19 +0200

    Fixes NaN issue at heightmap rasterization

--- shaders/makeHeightmap.glsl
+++ shaders/makeHeightmap.glsl
@@ -25,5 +25,5 @@ void main() {
     double heightRepresentation = max(double(sums[coordinate]) / double(counts[coordinate]), double(0));
     double height = heightRepresentation / (double(~0u) / double(counts[coordinate]));
 
-    heights[coordinate] = height;
+    heights[coordinate] = max(height, double(0.));
 }
