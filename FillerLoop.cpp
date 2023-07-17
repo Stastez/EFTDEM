9e691535986c83bb0d6a3f0964296e88dc1b4832
Sandro Steeger <sandro.steeger.stuff@gmail.com>
2023-07-17 22:01:44 +0200

    Documents FillerLoop

--- FillerLoop.cpp
+++ FillerLoop.cpp
@@ -4,14 +4,26 @@
 #include <chrono>
 #include <iostream>
 
+/**
+ * Constructs a FillerLoop that will apply all IHeightMapFillers given by distinctFillers sequentially. If any filler
+ * does not use OpenGL, the FillerLoop copies this behaviour outwardly.
+ * @param distinctFillers A vector containing IHeightMapFillers in the order they are supposed to be applied in
+ */
 FillerLoop::FillerLoop(std::vector<IHeightMapFiller *> distinctFillers) {
     FillerLoop::fillers = std::move(distinctFillers);
-    FillerLoop::stageUsesGPU = false;
-    for (auto filler : fillers) FillerLoop::stageUsesGPU |= filler->usesGPU();
+    FillerLoop::stageUsesGPU = true;
+    for (auto filler : fillers) FillerLoop::stageUsesGPU &= filler->usesGPU();
 }
 
 FillerLoop::~FillerLoop() noexcept = default;
 
+/**
+ * Fill the given heightMap using the previously provided IHeightMapFillers sequentially.
+ * @param map The heightMap to be filled
+ * @param generateOutput Whether or not to return a fully formed heightMap (including the actual heightData for each
+ * pixel). This may be set to false if the following stage uses OpenGL and can employ the already set buffers on the GPU
+ * @return A fully formed heightMap if generateOutput is true, a heightMap only containing all necessary metadata otherwise
+ */
 heightMap * FillerLoop::apply(heightMap *map, bool generateOutput) {
     auto result = map;
     auto start = std::chrono::high_resolution_clock::now();
