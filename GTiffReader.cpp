c34826abe77e0b3e4300b8512392518a7482440d
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2023-07-20 17:45:30 +0200

    implements Mean Square Error on GPU

--- GTiffReader.cpp
+++ GTiffReader.cpp
@@ -29,27 +29,38 @@ denormalizedHeightMap * GTiffReader::apply(bool generateOutput) {
 
     if (!generateOutput) return nullptr;
 
-    unsigned long resolutionX, resolutionY;
+    bool nrwScaling = false;
 
     GDALRegister_GTiff();
 
-    GDALAllRegister();//!!!!!!!!!! GDALRegister_GTiff();
+    //GDALAllRegister();//!!!!!!!!!! GDALRegister_GTiff();
     GDALDataset * dataset = (GDALDataset *) GDALOpen(sourceDEM.c_str(), GA_ReadOnly);
 
-    resolutionX = dataset->GetRasterXSize();
-    resolutionY = dataset->GetRasterYSize();
+    unsigned long resolutionX = dataset->GetRasterXSize(),
+        resolutionY = dataset->GetRasterYSize();
 
     auto rasterBand = dataset->GetRasterBand(1);
 
     auto heights = std::vector<double>(resolutionX * resolutionY);
-    (void)! rasterBand->RasterIO(GF_Read, 0, 0, resolutionX, resolutionY, heights.data(), resolutionX, resolutionY, GDT_Float64, 0, 0, nullptr);
+    (void)! rasterBand->RasterIO(GF_Read, 0, 0, (int) resolutionX, (int) resolutionY, heights.data(), (int) resolutionX, (int) resolutionY, GDT_Float64, 0, 0, nullptr);
 
-    double minHeight = std::numeric_limits<double>::max(), maxHeight = 0;
-    for (auto i = 0ul; i < resolutionY; i++){
-        minHeight = std::min(minHeight, heights.at(i));
-        maxHeight = std::max(maxHeight, heights.at(i));
+    //scale heights of Gtiff to be consistent with scale of Point-Cloud
+    if (nrwScaling){
+        for (auto i = 0ul; i < resolutionX * resolutionY; i++){
+            heights.at(i) = 63.639366 + heights.at(i) * 1.8559851;
+        }
     }
 
+    //Get minimum Height
+    double minHeight = std::numeric_limits<double>::max(), maxHeight = 0;
+    for (auto i = 0ul; i < resolutionY; i++){
+            minHeight = std::min(minHeight, heights.at(i));
+            maxHeight = std::max(maxHeight, heights.at(i));
+    }
+
+    //std::cout << "min: " << minHeight << " max: " << maxHeight << "\n";
+
+    //Flips map along the x-Accesses
     for (auto y = 0ul; y < (resolutionY / 2); y++){
         for (auto x = 0ul; x < resolutionX; x++) {
             auto coord1D = resolutionX * y + x;
